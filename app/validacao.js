b24d7f2ff7ae6a16ef0dd689c14b02020a0790bf
Rodrigo Dias <diasbass@hotmail.com>
2023-03-07 21:55:47 -0300

    Add btn new game

--- app/validacao.js
+++ app/validacao.js
@@ -17,6 +17,8 @@ function verificaSeOChutePossuiUmValorValido(chute) {
         document.body.innerHTML = `
             <h2>Você acertou!</h2>
             <h3>O número secreto era ${numeroSecreto}</h3>
+
+            <button id="jogar-novamente" class="btn-jogar">Jogar novamente</button>
         `
     } else if (numero > numeroSecreto) {
         elementoChute.innerHTML += `
@@ -37,3 +39,9 @@ function chuteForInvalido(numero) {
 function numeroForMaiorOuMenorQueOValorPermitido(numero){
     return numero > maiorValor || numero < menorValor
 }
+
+document.body.addEventListener('click', e => {
+  if (e.target.id == 'jogar-novamente') {
+      window.location.reload()
+  }
+})
